c615f2dcd88bc9e1f62de1f34754322c9bc5d812
thexman8888 <47992537+thexman8888@users.noreply.github.com>
2020-03-02 09:53:42 -0500

    Card class; Deal and shuffle for dealer;

---
+++ card.js
@@ -0,0 +1,11 @@
+let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+
+class Card {
+    constructor(suit, value){
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+// Don't commit
--- dealer.js
+++ dealer.js
@@ -1,16 +1,28 @@
-//Note to self, check syntax for error on first line
 class Dealer {
-    constructor(deck) {
-      this.hand = [];
+    constructor(deck){
+      this.dealerHand = [];
     }
-  }
-
 shuffle(){
-    let counter = this.deck.length, temp, i;
+  let temp;
+  let i;
+  let counter = this.deck.length, temp, i;
 
     while(counter) {
         i = Math.floor(Math.random() * counter--);
         temp = this.deck[counter];
-        this.deck[counter] = this
+        this.deck[counter] = this.deck[i];
+        this.deck[i] = temp;
     }
+    return this.deck;
 }
+
+Deal(){
+  let hand = [];
+  //what the player holds
+  while( hand.length < 2) {
+    hand.push(this.deck.pop());
+  }
+  return hand;
+}
+}
+console.log(deck.deal())
