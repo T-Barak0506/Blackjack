da1e578954a986fb5669c9c6612b5291682a8db3
T.Barak <56414950+T-Barak0506@users.noreply.github.com>
2020-03-02 10:04:26 -0500

    File fix

--- dealer.js
+++ dealer.js
@@ -3,3 +3,6 @@ class Dealer {
     this.hand = [];
   }
 }
+const deckOfCards = new Deck();
+deckOfCards.createDeck();
+console.log(deckOfCards.deck);
--- deck.js
+++ deck.js
@@ -16,5 +16,3 @@ class Deck {
     }
   }
 }
-
-// const x = 5;
