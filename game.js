07cfdae3df6676168f872d7ea615a730a1844fa7
T.Barak <56414950+T-Barak0506@users.noreply.github.com>
2020-03-09 07:44:21 -0400

    game.js fixes

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 class Game {
   constructor(player, dealer, deck) {
     this.discardPile = [];
-    this.player = player;
+    this.player = new Player();
     this.dealer = dealer;
     this.deck = deck;
   }
 
-  getHandValue(hand) {}
+  // getHandValue(hand) {}
 }
