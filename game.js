8cb62e8e02f693f49090d7fa3fc7e4fd12937fda
T.Barak <56414950+T-Barak0506@users.noreply.github.com>
2020-03-09 10:13:21 -0400

    .

--- game.js
+++ game.js
@@ -2,8 +2,7 @@ class Game {
   constructor(player, dealer, deck) {
     this.discardPile = [];
     this.player = new Player();
-    this.dealer = dealer;
-    this.deck = deck;
+    this.dealer = new Dealer();
   }
 
   // getHandValue(hand) {}
